88c9683a388b94c25623dee114742b1fa54a546a
RD osio <125887036+rond8@users.noreply.github.com>
2025-04-03 12:52:12 +0800

    Update bjs.js

--- bjs.js
+++ bjs.js
@@ -42,6 +42,8 @@ function blackOutScreen(duration, messages, additional) {
         blackout.style.opacity = 0;
         setTimeout(() => {
             document.body.removeChild(blackout);
+
+            alert('updated')
         }, 500);
     }, duration * 1000 - 500);
 }
