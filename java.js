2b3815a12b742ff4176fbea4576e5fc6fa6cafd8
RD osio <125887036+rond8@users.noreply.github.com>
2025-04-03 09:42:38 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,13 +1,4 @@
-alert('lala')
-confirm('lala')
-confirm('lala')
  document.body.style.backgroundColor='black';
-let m = "you click this button ";
-let times = 0;
 
-function gg() {
-  times++;
-  confirm(m + times + " times");
-}
 
-gg();
+
